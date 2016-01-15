d7e01884c8c23bdc8956989aabc0bb5c9e1cfd67
TonyPan <454191028@qq.com>
2016-01-15 20:44:38 +0800

    实现了读取手机系统联系人的功能。

--- src/com/tonypan/contacts/MainActivity.java
+++ src/com/tonypan/contacts/MainActivity.java
@@ -1,14 +1,45 @@
 package com.tonypan.contacts;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends ActionBarActivity {
 
-	private String abc = "";
+	private List<String> contactsList = new ArrayList<String>();
+	private ListView contactsView;
+	private ArrayAdapter<String> adapter;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		contactsView = (ListView) findViewById(R.id.contacts_view);
+		
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contactsList);
+		contactsView.setAdapter(adapter);
+		readContacts();
+		
+	}
+	
+	private void readContacts(){
+		Cursor cursor = null;
+		//查询联系人
+		cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+				null, null, null, null);
+		while (cursor.moveToNext()) {
+			//获取联系人姓名
+			String displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+			//获取联系人号码
+			String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+			contactsList.add(displayName + "\n" + number); 
+		}
+		cursor.close();
 	}
 }
