cf28b0ed7698f385eb1860281dfcb81a252d4f91
Tony_Pan <454191028@qq.com>
2016-01-15 20:03:10 +0800

    Update MainActivity.java

--- src/com/tonypan/contacts/MainActivity.java
+++ src/com/tonypan/contacts/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 
 public class MainActivity extends ActionBarActivity {
 
+	private String abc = "";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
